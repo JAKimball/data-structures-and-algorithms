842874a68bbc4ac30f788ae97b1ed85f2056e670
Jonathan A. Kimball <jonathan.kimball64@gmail.com>
2019-09-26 16:16:55 -0700

    All stretch goals from all challenges done!

--- code-challenges/reg-ex-1/challenges-04.test.js
+++ code-challenges/reg-ex-1/challenges-04.test.js
@@ -113,9 +113,7 @@ Hint: All of these words end with the letters "ells".
 
 const seashells = 'She sells seashells by the seashore. The shells she sells are surely seashells. So if she sells shells on the seashore, I\'m sure she sells seashore shells.';
 
-const findShells = (str) => {
-  // Solution code here...
-};
+const findShells = (str) => str.match(/\w*ells\b/ig);
 
 /* ------------------------------------------------------------------------------------------------
 TESTS
@@ -216,7 +214,7 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   test('It should return an array of instances of "sells", shells", and "seashells"', () => {
     expect(findShells(seashells)).toStrictEqual(['sells', 'seashells', 'shells', 'sells', 'seashells', 'sells', 'shells', 'sells', 'shells']);
     expect(findShells(seashells).length).toStrictEqual(9);
