440a7fc937664626c7a343a89d90d40ca2621e88
Jonathan A. Kimball <jonathan.kimball64@gmail.com>
2019-11-11 09:05:41 -0800

    Readme complete

--- code-challenges/data-structures/tree/tree.js
+++ code-challenges/data-structures/tree/tree.js
@@ -1,2 +1,56 @@
 'use strict';
 
+class Node {
+  constructor() {
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinaryTree {
+  constructor() {
+    this.root = null;
+  }
+
+  // * `preOrder()` - returns an array of the values, ordered in pre-order (root, left, right).
+  preOrder() {
+
+  }
+
+  // * `inOrder()` - returns an array of the values, ordered in-order (left, root, right).
+
+  inOrder() {
+
+  }
+  // * `postOrder()` - returns an array of the values, ordered in post-order (left, right, root).
+
+  postOrder() {
+
+  }
+
+  // * `breadthFirst()` - traverse the input tree using a Breadth-first approach; print every visited node’s value.
+  breadthFirst() {
+
+  }
+
+  // * `findMaximumValue()` - Instance method that returns the maximum value in the tree assuming.  Assumes the values stored are numeric.
+  findMaximumValue() {
+
+  }
+
+}
+
+class BinarySearchTree extends BinaryTree {
+
+  // `add(value)` - Adds a new node with the given numeric value to the tree in the correct location.
+  add(value) {
+
+  }
+
+  // `contains(value)` - Returns a boolean indicating whether the value is in the tree.
+  contains(value) {
+
+  }
+
+}
+
